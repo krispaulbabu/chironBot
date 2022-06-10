9c938c5210db9beecd928bf2eca5c9cb93fe8c1c
krispaulbabu <krispaulbabu.krispaulbabu@students.mq.edu.au>
2022-06-11 01:21:46 +1000

    prank time over

--- picker.py
+++ picker.py
@@ -13,6 +13,13 @@ def returnLine(comment):
 
 # Method to replace name with author's name 
 lines=[
-  "Fuck you u/Author"
+  "Im sure the boy can learn", 
+  "Ah, good, u/Author, now we have four for Pinochle",
+  "I must say Percy, Im glad to see you alive",
+  "What you learn from me, is vitally important. I expect you to treat it as such. I will accept only the best from you, u/Author.",
+  "What ho, u/Author!",
+  "Ah, that would be my pen. Please bring your own writing utensil in the future, u/Author",
+  "We would only make matters worse by rushing him, We need u/Author to mature more.",
+  "Nothing, My nerves haven't been right since the winter solstice."
   ]
 
